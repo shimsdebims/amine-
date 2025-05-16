5fd30a82fa58c1a5709a327440504029dadfa79f
shimsdebims <shimasarah777@gmail.com>
2025-05-16 17:47:42 +0000

    idk

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { AuthProvider } from './src/context/AuthContext';
-import AppNavigator from './src/navigation/appNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
 import { colors } from './src/constants/colors';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider as StoreProvider } from 'react-redux';
+import store from './src/store/reducers/store';
 
-export default function App() {
+function App() {
   return (
-    <AuthProvider>
-      <PaperProvider>
-        <AppNavigator />
-      </PaperProvider>
-    </AuthProvider>
+    <StoreProvider store={store}>
+      <NavigationContainer>
+        <AuthProvider>
+          <PaperProvider theme={{
+            colors: {
+              primary: colors.primary,
+              accent: colors.secondary,
+            },
+          }}>
+            <AppNavigator />
+          </PaperProvider>
+        </AuthProvider>
+      </NavigationContainer>
+    </StoreProvider>
   );
 }
+
+AppRegistry.registerComponent('servicemarketplace-bf', () => App);
+
+export default App;
