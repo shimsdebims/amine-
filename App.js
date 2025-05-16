32a3625e3d97ddc2d937550ef95fc050dabe9ed2
shimsdebims <shimasarah777@gmail.com>
2025-05-16 12:02:01 +0000

    splash and mini corrections

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { AuthProvider } from './src/context/AuthContext';
-import AppNavigator from './src/navigation/AppNavigator';
+import AppNavigator from './src/navigation/appNavigator';
 import { colors } from './src/constants/colors';
 
 export default function App() {
