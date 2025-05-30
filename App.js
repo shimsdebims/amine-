574ab9122259f607c71a8ff12802f568ed04beaa
shimsdebims <shimasarah777@gmail.com>
2025-05-30 15:27:27 +0000

    app error

--- App.js
+++ App.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { NavigationContainer } from '@react-navigation/native';
-import store from './src/redux/store';
-import AppNavigator from './src/navigation/AppNavigator';
-
-export default function App() {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </Provider>
-  );
-}
-
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
